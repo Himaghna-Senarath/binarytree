ae30435f4a0f5353ad54efcc268bdafbfa94f03e
Himaghna Senarath <himaghnasenarath2007@gmail.com>
2022-04-14 17:55:31 +0530

    pg view

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import { binaryTree, AVLTree } from './BST'
 
 import { useEffect } from 'react';
 
+import ReactGA from "react-ga4";
+
+if (!window.location.href.includes("localhost")) {
+  ReactGA.initialize("G-0X9C7QGH9F");
+  ReactGA.send("pageview");
+}
+
 function App() {
   return (
     <div className="App">
